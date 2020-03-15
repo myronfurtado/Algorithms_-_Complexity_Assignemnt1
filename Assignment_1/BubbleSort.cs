87242d4358a95f928678269a7573c9223243f92c
myronfurtado <61276675+myronfurtado@users.noreply.github.com>
2020-03-15 01:44:34 +0000

    Add files via upload

---
+++ Assignment_1/BubbleSort.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Assignment_1
+{
+    public class BubbleSort
+    {
+
+        //public static int totalSteps = 0;
+        public int i = 0;
+
+        //public int stepCounter = 0;
+
+        public static void BubbleAscendingSort(int[] unsortedAscendingsort)
+        {
+            int stepCounter = 0;// counts the number steps  the sort algorithhm takes
+            int temp;
+            //When looping we deal with 1 less than the lenght of the array the current number with the one next to it
+            //It is unnecessary to go to the last number as it will be the last one
+
+            for (int i = 0; i < unsortedAscendingsort.Length - 1; i++)
+            {
+                //I soved 'i' from the right side
+                //'i' should not be added to the last element as it has already been moved to the sopt on its right
+                for (int j = 0; j < unsortedAscendingsort.Length - (1 + i); j++)
+                {
+                    stepCounter = stepCounter + 1;
+
+                    if (unsortedAscendingsort[j] > unsortedAscendingsort[j + 1])
+                    {
+                        stepCounter = stepCounter + 1;
+                        //if the current number is greater than the one to the right they will swap location
+                        temp = unsortedAscendingsort[j + 1];
+                        unsortedAscendingsort[j + 1] = unsortedAscendingsort[j];
+                        unsortedAscendingsort[j] = temp;
+
+                    }
+
+                }
+
+            }
+            Console.WriteLine($"The Bubble Sort took {stepCounter} steps\n");
+        }
+
+
+        //code for the descending sort display
+        public static void BubbleDescendingSort(int[] unsorteddescendingsort)
+        {
+            int stepCounter = 0;
+            int temp;
+            //When looping we deal with 1 less than the lenght of the array the current number with the one next to it
+            //It is unnecessary to go to the last number as it will be the last one
+
+            for (int i = unsorteddescendingsort.Length; i > 0; i--)
+
+            {
+                //solved 'i' from the right side
+                //'i' should not be added to the last element as it has already been moved to the sopt on its right
+                for (int j = unsorteddescendingsort.Length - 1; j >= 0; j--)
+                {
+                    //when J is not equal to zero it will check the nu,ber to the right and move the bigger number to the right
+                    if (j != 0)
+                    {
+                        if (unsorteddescendingsort[j - 1] < unsorteddescendingsort[j])
+                        {
+                            stepCounter = stepCounter + 1;
+                            //if the current number is smaller than the one to the right they will swap location
+                            temp = unsorteddescendingsort[j];
+                            unsorteddescendingsort[j] = unsorteddescendingsort[j - 1];
+                            unsorteddescendingsort[j - 1] = temp;
+                        }
+
+                    }
+                }
+
+
+            }
+            Console.WriteLine($"Bubble Sort took {stepCounter} steps\n");
+        }
+
+    }
+}
---
+++ Assignment_1/LinearSearch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_1
+{
+    class StepCounter
+    {
+        public static int Stepcounter = 0;
+    }
+    class LinearSearch1
+    {
+
+
+        public static int LinearSearch(int[] array, int r)
+        { // code is used to search for r in int[] array selected by the user
+
+            int n = array.Length;
+
+            for (int j = 0; j < n; j++)
+            {
+                //Stepcounter is used to count the number of steps performed by the algorithm
+                StepCounter.Stepcounter++;
+                if (array[j] == r)
+                {
+                    return j;
+                }
+               
+            }
+            return -1;
+            // if r is present in the array then return it index location
+            //the rest of the code is called in Program.cs
+
+        }
+
+
+    }
+}
+
+
+
